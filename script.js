5577c64cfdebadcf62c64c6be345546cad2e5bd5
Leillac88 <leillacarvalho88@gmail.com>
2025-07-09 11:56:30 -0300

    button functions in js and css

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ const gameSelect = document.getElementById('gameSelect')
 const questionInput = document.getElementById('questionInput')
 const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
-const form = document.getElementById('form')
+const form = document.getElementById('form');
+
+const enviarFormulario = async (event) => {
+  event.preventDefault()
+  const apiKey = apiKeyInput.value
+  const game = gameSelect.value
+  const question = questionInput.value
+
+  if (apiKey == '' || game == '' || question == '') {
+    alert('Por favor, preencha todos os campos')
+    return
+  }
+
+  askButton.disabled = true
+  askButton.textContent = 'Perguntando...'
+  askButton.classList.add('loading')
+
+  try {
+    const text = await perguntarAI(question, game, apiKey)
+    aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+    aiResponse.classList.remove('hidden')
+  } catch (error) {
+    console.log('Erro: ', error)
+  } finally {
+    askButton.disabled = false
+    askButton.textContent = "Perguntar"
+    askButton.classList.remove('loading')
+  }
+}
+
+form.addEventListener('submit', enviarFormulario)
